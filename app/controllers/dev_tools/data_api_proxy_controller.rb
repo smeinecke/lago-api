d9fbd9c79efccca719d1086cafb57aa4c783bfdf
Stefan Meinecke <meinecke@greensec.de>
2025-04-16 00:55:45 +0200

    add missing module config.

--- app/controllers/dev_tools/data_api_proxy_controller.rb
+++ app/controllers/dev_tools/data_api_proxy_controller.rb
@@ -1,106 +1,108 @@
 # frozen_string_literal: true
 
-class DataApiProxyController < ApplicationController
-  # Proxy for /mrrs/:organization_id/
-  def mrrs
-    result = LagoSchema.execute(
-      <<~GRAPHQL,
-        variables: { organizationId: params[:organization_id] },
-        context: graphql_context
-      query Mrrs($organizationId: ID!) {
-        dataApiMrrs(organizationId: $organizationId) {
-          ... # Add the fields you need
+module DevTools
+  class DataApiProxyController < ApplicationController
+    # Proxy for /mrrs/:organization_id/
+    def mrrs
+      result = LagoSchema.execute(
+        <<~GRAPHQL,
+          variables: { organizationId: params[:organization_id] },
+          context: graphql_context
+        query Mrrs($organizationId: ID!) {
+          dataApiMrrs(organizationId: $organizationId) {
+            ... # Add the fields you need
+          }
         }
-      }
-      GRAPHQL
-    )
-    render json: result['data']['dataApiMrrs']
-  end
+        GRAPHQL
+      )
+      render json: result['data']['dataApiMrrs']
+    end
 
-  # Proxy for /mrrs/:organization_id/plans/
-  def mrrs_plans
-    result = LagoSchema.execute(
-      <<~GRAPHQL,
-        variables: { organizationId: params[:organization_id] },
-        context: graphql_context
-      query MrrsPlans($organizationId: ID!) {
-        dataApiMrrsPlans(organizationId: $organizationId) {
-          ... # Add the fields you need
+    # Proxy for /mrrs/:organization_id/plans/
+    def mrrs_plans
+      result = LagoSchema.execute(
+        <<~GRAPHQL,
+          variables: { organizationId: params[:organization_id] },
+          context: graphql_context
+        query MrrsPlans($organizationId: ID!) {
+          dataApiMrrsPlans(organizationId: $organizationId) {
+            ... # Add the fields you need
+          }
         }
-      }
-      GRAPHQL
-    )
-    render json: result['data']['dataApiMrrsPlans']
-  end
+        GRAPHQL
+      )
+      render json: result['data']['dataApiMrrsPlans']
+    end
 
-  # Proxy for /revenue_streams/:organization_id/
-  def revenue_streams
-    result = LagoSchema.execute(
-      <<~GRAPHQL,
-        variables: { organizationId: params[:organization_id] },
-        context: graphql_context
-      query RevenueStreams($organizationId: ID!) {
-        dataApiRevenueStreams(organizationId: $organizationId) {
-          ... # Add the fields you need
+    # Proxy for /revenue_streams/:organization_id/
+    def revenue_streams
+      result = LagoSchema.execute(
+        <<~GRAPHQL,
+          variables: { organizationId: params[:organization_id] },
+          context: graphql_context
+        query RevenueStreams($organizationId: ID!) {
+          dataApiRevenueStreams(organizationId: $organizationId) {
+            ... # Add the fields you need
+          }
         }
-      }
-      GRAPHQL
-    )
-    render json: result['data']['dataApiRevenueStreams']
-  end
+        GRAPHQL
+      )
+      render json: result['data']['dataApiRevenueStreams']
+    end
 
-  # Proxy for /revenue_streams/:organization_id/plans/
-  def revenue_streams_plans
-    result = LagoSchema.execute(
-      <<~GRAPHQL,
-        variables: { organizationId: params[:organization_id] },
-        context: graphql_context
-      query RevenueStreamsPlans($organizationId: ID!) {
-        dataApiRevenueStreamsPlans(organizationId: $organizationId) {
-          ... # Add the fields you need
+    # Proxy for /revenue_streams/:organization_id/plans/
+    def revenue_streams_plans
+      result = LagoSchema.execute(
+        <<~GRAPHQL,
+          variables: { organizationId: params[:organization_id] },
+          context: graphql_context
+        query RevenueStreamsPlans($organizationId: ID!) {
+          dataApiRevenueStreamsPlans(organizationId: $organizationId) {
+            ... # Add the fields you need
+          }
         }
-      }
-      GRAPHQL
-    )
-    render json: result['data']['dataApiRevenueStreamsPlans']
-  end
+        GRAPHQL
+      )
+      render json: result['data']['dataApiRevenueStreamsPlans']
+    end
 
-  # Proxy for /revenue_streams/:organization_id/customers/
-  def revenue_streams_customers
-    result = LagoSchema.execute(
-      <<~GRAPHQL,
-        variables: { organizationId: params[:organization_id] },
-        context: graphql_context
-      query RevenueStreamsCustomers($organizationId: ID!) {
-        dataApiRevenueStreamsCustomers(organizationId: $organizationId) {
-          ... # Add the fields you need
+    # Proxy for /revenue_streams/:organization_id/customers/
+    def revenue_streams_customers
+      result = LagoSchema.execute(
+        <<~GRAPHQL,
+          variables: { organizationId: params[:organization_id] },
+          context: graphql_context
+        query RevenueStreamsCustomers($organizationId: ID!) {
+          dataApiRevenueStreamsCustomers(organizationId: $organizationId) {
+            ... # Add the fields you need
+          }
         }
-      }
-      GRAPHQL
-    )
-    render json: result['data']['dataApiRevenueStreamsCustomers']
-  end
+        GRAPHQL
+      )
+      render json: result['data']['dataApiRevenueStreamsCustomers']
+    end
 
-  # Proxy for /usages/:organization_id/invoiced/
-  def usages_invoiced
-    result = LagoSchema.execute(
-      <<~GRAPHQL,
-        variables: { organizationId: params[:organization_id] },
-        context: graphql_context
-      query UsagesInvoiced($organizationId: ID!) {
-        dataApiUsagesInvoiced(organizationId: $organizationId) {
-          ... # Add the fields you need
+    # Proxy for /usages/:organization_id/invoiced/
+    def usages_invoiced
+      result = LagoSchema.execute(
+        <<~GRAPHQL,
+          variables: { organizationId: params[:organization_id] },
+          context: graphql_context
+        query UsagesInvoiced($organizationId: ID!) {
+          dataApiUsagesInvoiced(organizationId: $organizationId) {
+            ... # Add the fields you need
+          }
         }
-      }
-      GRAPHQL
-    )
-    render json: result['data']['dataApiUsagesInvoiced']
-  end
+        GRAPHQL
+      )
+      render json: result['data']['dataApiUsagesInvoiced']
+    end
 
-  private
+    private
 
-  def graphql_context
-    # Add any context needed for authentication, etc.
-    {}
+    def graphql_context
+      # Add any context needed for authentication, etc.
+      {}
+    end
   end
 end
